5179bb381a9d2c077fdba5d49b2991152b0c1cde
Crimson Bastille <CrimsonBastille@proton.me>
2025-05-29 22:17:25 -0700

    ### Commit Message
    Update devDependencies and clean up Vite configuration
    
    Upgraded various development dependencies to their latest versions for improved compatibility and security. This includes updates to Babel, ESLint, SWC, TypeScript, Vite, and associated plugins. These updates are part of ongoing maintenance to ensure compatibility with the latest libraries and tools.
    
    Removed unused plugins and consolidated the build configuration in `vite.config.ts`:
    
    - Removed `rollup-plugin-preserve-shebang` and `rollup-plugin-visualizer` to streamline the build configuration.
    - Cleaned up the Vite configuration by eliminating unnecessary comment and imports, optimizing for current usage.
    
    These changes reduce unused dependencies in the build process and ensure configurations are up-to-date with the project’s current needs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
 import replace from '@rollup/plugin-replace';
-// import { visualizer } from 'rollup-plugin-visualizer';
 import { execSync } from 'child_process';
-import shebang from 'rollup-plugin-preserve-shebang';
 import dts from 'vite-plugin-dts';
 
 let gitInfo = {
@@ -92,11 +90,6 @@ export default defineConfig({
                     entryFileNames: '[name].cjs',
                     exports: 'named'
                 }
-            ],
-            plugins: [
-                shebang({
-                    shebang: '#!/usr/bin/env node',
-                }),
             ],
         },
         // Make sure Vite generates ESM-compatible code
